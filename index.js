b0cc0cec771c963eace9300b24282916069fe0b7
yryryryrz <poison.13h12@gmail.com>
2023-09-19 00:10:41 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,304 @@
+const productsSwe = [
+  {
+    id: 1,
+    img: "https://down-vn.img.susercontent.com/file/vn-11134207-7qukw-lj6kqx2w9vrg28",
+    name: "Áo thun local brand WAVELINES TEE form rộng tay lỡ - Thương hiệu SWEVN",
+    priceOld: "450,000",
+    priceCurrent: "350,000",
+    sold: "đã bán 120",
+    brand: "SWE",
+    origin: "Vietnam",
+    salepercent: "10%",
+  },
+  {
+    id: 2,
+    img: "https://down-vn.img.susercontent.com/file/vn-11134207-7qukw-lj6kqx2w9vrg28",
+    name: "Áo thun local brand WAVELINES TEE form rộng tay lỡ - Thương hiệu SWEVN",
+    priceOld: "450,000",
+    priceCurrent: "350,000",
+    sold: "đã bán 120",
+    brand: "SWE",
+    origin: "Vietnam",
+    salepercent: "10%",
+  },
+  {
+    id: 3,
+    img: "https://down-vn.img.susercontent.com/file/vn-11134207-7qukw-lj6kqx2w9vrg28",
+    name: "Áo thun local brand WAVELINES TEE form rộng tay lỡ - Thương hiệu SWEVN",
+    priceOld: "450,000",
+    priceCurrent: "350,000",
+    sold: "đã bán 120",
+    brand: "SWE",
+    origin: "Vietnam",
+    salepercent: "10%",
+  },
+  {
+    id: 4,
+    img: "https://down-vn.img.susercontent.com/file/vn-11134207-7qukw-lj6kqx2w9vrg28",
+    name: "Áo thun local brand WAVELINES TEE form rộng tay lỡ - Thương hiệu SWEVN",
+    priceOld: "450,000",
+    priceCurrent: "350,000",
+    sold: "đã bán 120",
+    brand: "SWE",
+    origin: "Vietnam",
+    salepercent: "10%",
+  },
+  {
+    id: 5,
+    img: "https://down-vn.img.susercontent.com/file/vn-11134207-7qukw-lj6kqx2w9vrg28",
+    name: "Áo thun local brand WAVELINES TEE form rộng tay lỡ - Thương hiệu SWEVN",
+    priceOld: "450,000",
+    priceCurrent: "350,000",
+    sold: "đã bán 120",
+    brand: "SWE",
+    origin: "Vietnam",
+    salepercent: "10%",
+  },
+  {
+    id: 6,
+    img: "https://down-vn.img.susercontent.com/file/vn-11134207-7qukw-lj6kqx2w9vrg28",
+    name: "Áo thun local brand WAVELINES TEE form rộng tay lỡ - Thương hiệu SWEVN",
+    priceOld: "450,000",
+    priceCurrent: "350,000",
+    sold: "đã bán 120",
+    brand: "SWE",
+    origin: "Vietnam",
+    salepercent: "10%",
+  },
+  {
+    id: 7,
+    img: "https://down-vn.img.susercontent.com/file/vn-11134207-7qukw-lj6kqx2w9vrg28",
+    name: "Áo thun local brand WAVELINES TEE form rộng tay lỡ - Thương hiệu SWEVN",
+    priceOld: "450,000",
+    priceCurrent: "350,000",
+    sold: "đã bán 120",
+    brand: "SWE",
+    origin: "Vietnam",
+    salepercent: "10%",
+  },
+  {
+    id: 8,
+    img: "https://down-vn.img.susercontent.com/file/vn-11134207-7qukw-lj6kqx2w9vrg28",
+    name: "Áo thun local brand WAVELINES TEE form rộng tay lỡ - Thương hiệu SWEVN",
+    priceOld: "450,000",
+    priceCurrent: "350,000",
+    sold: "đã bán 120",
+    brand: "SWE",
+    origin: "Vietnam",
+    salepercent: "10%",
+  },
+  {
+    id: 9,
+    img: "https://down-vn.img.susercontent.com/file/vn-11134207-7qukw-lj6kqx2w9vrg28",
+    name: "Áo thun local brand WAVELINES TEE form rộng tay lỡ - Thương hiệu SWEVN",
+    priceOld: "450,000",
+    priceCurrent: "350,000",
+    sold: "đã bán 120",
+    brand: "SWE",
+    origin: "Vietnam",
+    salepercent: "10%",
+  },
+  {
+    id: 10,
+    img: "https://down-vn.img.susercontent.com/file/vn-11134207-7qukw-lj6kqx2w9vrg28",
+    name: "Áo thun local brand WAVELINES TEE form rộng tay lỡ - Thương hiệu SWEVN",
+    priceOld: "450,000",
+    priceCurrent: "350,000",
+    sold: "đã bán 120",
+    brand: "SWE",
+    origin: "Vietnam",
+    salepercent: "10%",
+  },
+  {
+    id: 11,
+    img: "https://down-vn.img.susercontent.com/file/vn-11134207-7qukw-lj6kqx2w9vrg28",
+    name: "Áo thun local brand WAVELINES TEE form rộng tay lỡ - Thương hiệu SWEVN",
+    priceOld: "450,000",
+    priceCurrent: "350,000",
+    sold: "đã bán 120",
+    brand: "SWE",
+    origin: "Vietnam",
+    salepercent: "10%",
+  },
+  {
+    id: 12,
+    img: "https://down-vn.img.susercontent.com/file/vn-11134207-7qukw-lj6kqx2w9vrg28",
+    name: "Áo thun local brand WAVELINES TEE form rộng tay lỡ - Thương hiệu SWEVN",
+    priceOld: "450,000",
+    priceCurrent: "350,000",
+    sold: "đã bán 120",
+    brand: "SWE",
+    origin: "Vietnam",
+    salepercent: "10%",
+  },
+  {
+    id: 13,
+    img: "https://down-vn.img.susercontent.com/file/vn-11134207-7qukw-lj6kqx2w9vrg28",
+    name: "Áo thun local brand WAVELINES TEE form rộng tay lỡ - Thương hiệu SWEVN",
+    priceOld: "450,000",
+    priceCurrent: "350,000",
+    sold: "đã bán 120",
+    brand: "SWE",
+    origin: "Vietnam",
+    salepercent: "10%",
+  },
+  {
+    id: 14,
+    img: "https://down-vn.img.susercontent.com/file/vn-11134207-7qukw-lj6kqx2w9vrg28",
+    name: "Áo thun local brand WAVELINES TEE form rộng tay lỡ - Thương hiệu SWEVN",
+    priceOld: "450,000",
+    priceCurrent: "350,000",
+    sold: "đã bán 120",
+    brand: "SWE",
+    origin: "Vietnam",
+    salepercent: "10%",
+  },
+  {
+    id: 15,
+    img: "https://down-vn.img.susercontent.com/file/vn-11134207-7qukw-lj6kqx2w9vrg28",
+    name: "Áo thun local brand WAVELINES TEE form rộng tay lỡ - Thương hiệu SWEVN",
+    priceOld: "450,000",
+    priceCurrent: "350,000",
+    sold: "đã bán 120",
+    brand: "SWE",
+    origin: "Vietnam",
+    salepercent: "10%",
+  },
+  {
+    id: 16,
+    img: "https://down-vn.img.susercontent.com/file/vn-11134207-7qukw-lj6kqx2w9vrg28",
+    name: "Áo thun local brand WAVELINES TEE form rộng tay lỡ - Thương hiệu SWEVN",
+    priceOld: "450,000",
+    priceCurrent: "350,000",
+    sold: "đã bán 120",
+    brand: "SWE",
+    origin: "Vietnam",
+    salepercent: "10%",
+  },
+  {
+    id: 17,
+    img: "https://down-vn.img.susercontent.com/file/vn-11134207-7qukw-lj6kqx2w9vrg28",
+    name: "Áo thun local brand WAVELINES TEE form rộng tay lỡ - Thương hiệu SWEVN",
+    priceOld: "450,000",
+    priceCurrent: "350,000",
+    sold: "đã bán 120",
+    brand: "SWE",
+    origin: "Vietnam",
+    salepercent: "10%",
+  },
+  {
+    id: 18,
+    img: "https://down-vn.img.susercontent.com/file/vn-11134207-7qukw-lj6kqx2w9vrg28",
+    name: "Áo thun local brand WAVELINES TEE form rộng tay lỡ - Thương hiệu SWEVN",
+    priceOld: "450,000",
+    priceCurrent: "350,000",
+    sold: "đã bán 120",
+    brand: "SWE",
+    origin: "Vietnam",
+    salepercent: "10%",
+  },
+  {
+    id: 19,
+    img: "https://down-vn.img.susercontent.com/file/vn-11134207-7qukw-lj6kqx2w9vrg28",
+    name: "Áo thun local brand WAVELINES TEE form rộng tay lỡ - Thương hiệu SWEVN",
+    priceOld: "450,000",
+    priceCurrent: "350,000",
+    sold: "đã bán 120",
+    brand: "SWE",
+    origin: "Vietnam",
+    salepercent: "10%",
+  },
+  {
+    id: 20,
+    img: "https://down-vn.img.susercontent.com/file/vn-11134207-7qukw-lj6kqx2w9vrg28",
+    name: "Áo thun local brand WAVELINES TEE form rộng tay lỡ - Thương hiệu SWEVN",
+    priceOld: "450,000",
+    priceCurrent: "350,000",
+    sold: "đã bán 120",
+    brand: "SWE",
+    origin: "Vietnam",
+    salepercent: "10%",
+  },
+  {
+    id: 21,
+    img: "https://down-vn.img.susercontent.com/file/vn-11134207-7qukw-lj6kqx2w9vrg28",
+    name: "Áo thun local brand WAVELINES TEE form rộng tay lỡ - Thương hiệu SWEVN",
+    priceOld: "450,000",
+    priceCurrent: "350,000",
+    sold: "đã bán 120",
+    brand: "SWE",
+    origin: "Vietnam",
+    salepercent: "10%",
+  },
+  {
+    id: 22,
+    img: "https://down-vn.img.susercontent.com/file/vn-11134207-7qukw-lj6kqx2w9vrg28",
+    name: "Áo thun local brand WAVELINES TEE form rộng tay lỡ - Thương hiệu SWEVN",
+    priceOld: "450,000",
+    priceCurrent: "350,000",
+    sold: "đã bán 120",
+    brand: "SWE",
+    origin: "Vietnam",
+    salepercent: "10%",
+  },
+];
+
+let productIncart = localStorage.getItem("productsSwe")
+  ? JSON.parse(localStorage.getItem("productsSwe"))
+  : [];
+
+function saveToLocalStorage() {
+  localStorage.setItem("productsSwe", JSON.stringify(productIncart));
+}
+function renderProducts() {
+  let data = ``;
+  productsSwe.map((value) => {
+    data += `
+        <div class="grid__column-2-4">
+  <a class="home-product-item" href="./product.html">
+    <div
+      class="home-product-item__img"
+      style="background-image: url(${value.img});"
+    ></div>
+    <h4 class="home-product-item__name">
+      ${value.name}
+    </h4>
+    <div class="home-product-item__price">
+      <span class="home-product-item__price-old">${value.priceOld}</span>
+      <span class="home-product-item__price-current">${value.priceCurrent}</span>
+    </div>
+    <div class="home-product-item__action">
+      <span class="home-product-item__like home-product-item__like--liked">
+        <i class="home-product-item__like-icon-empty fa-regular fa-heart"></i>
+        <i class="home-product-item__like-icon-fill fa-solid fa-heart"></i>
+      </span>
+      <div class="home-product-item__rating">
+        <i class="home-product-item--star--gold fa-solid fa-star"></i>
+        <i class="home-product-item--star--gold fa-solid fa-star"></i>
+        <i class="home-product-item--star--gold fa-solid fa-star"></i>
+        <i class="home-product-item--star--gold fa-solid fa-star"></i>
+        <i class="home-product-item fa-solid fa-star"></i>
+      </div>
+      <span class="home-product-item__sold">${value.sold}</span>
+    </div>
+    <div class="home-product-item__origin">
+      <span class="home-product-item__brand">${value.brand}</span>
+      <span class="home-product-item__origin-name">${value.origin}</span>
+    </div>
+    <div class="home-product-item__favourite">
+      <i class="fa-solid fa-check"></i>
+      <span>Yêu thích</span>
+    </div>
+    <div class="home-product-item__sale-off">
+      <span class="home-product-item__sale-off-percent">${value.salepercent}</span>
+      <span class="home-product-item__sale-off-label">GIẢM</span>
+    </div>
+  </a>
+</div>
+`;
+  });
+  document.querySelector("#products").innerHTML = data;
+}
+renderProducts();
+
+
